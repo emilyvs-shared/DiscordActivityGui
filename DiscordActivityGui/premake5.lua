315df6760c495ca94d86036165b49e5e4c708b64
troy van sleeuwen <troyvansleeuwen@gmail.com>
2022-05-01 19:16:05 +0200

    setup done

--- DiscordActivityGui/premake5.lua
+++ DiscordActivityGui/premake5.lua
@@ -1,4 +1,4 @@
-project "WalnutApp"
+project "DiscordActivityGui"
    kind "ConsoleApp"
    language "C++"
    cppdialect "C++17"
--- premake5.lua
+++ premake5.lua
@@ -1,10 +1,10 @@
 -- premake5.lua
-workspace "WalnutApp"
+workspace "DiscordActivityGui"
    architecture "x64"
    configurations { "Debug", "Release", "Dist" }
-   startproject "WalnutApp"
+   startproject "DiscordActivityGui"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 include "Walnut/WalnutExternal.lua"
 
-include "WalnutApp"
+include "DiscordActivityGui"
